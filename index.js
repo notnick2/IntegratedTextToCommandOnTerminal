4f2cc82bea052aad93f6fe54f29b33ecb5ea7666
notnick2 <varun024123@gmail.com>
2024-03-08 18:03:07 +0530

    checkpoint

--- index.js
+++ index.js
@@ -8,13 +8,36 @@ app.use(bodyParser.json());
 
 app.post('/get-commands', (req, res) => {
   const { prompt } = req.body;
+  console.log(prompt)
 
-  // Add your logic to generate commands based on the prompt
-  const commands = [
-    'echo "$(pwd)"',
-    'cd ..'
+  commands=[
+    "cd ~/Desktop/100xdevs",
+    "npx create-next-app my-nextjs-app --yes",
+    "cd my-nextjs-app",
+    "npm init -y",
+    "npm install express",
+    "npx prisma init --yes",
+    "npm install @prisma/client",
+    "npm install concurrently",
+    "npm install nodemon",
+    "echo 'node_modules' > .gitignore",
+    "mkdir test",
+    "cd test",
+    "npx create-next-app my-test-app --yes",
+    "cd my-test-app",
+    "npm init -y",
+    "npm install express",
+    "npx prisma init --yes",
+    "npm install @prisma/client",
+    "npm install concurrently",
+    "npm install nodemon",
+    "echo 'node_modules' > .gitignore",
+    "cd ../..",
+    "touch index.js server.js",
+  ]
+  
 
-  ];
+
 
   const summary = 'The "Hello, World!" program has been compiled and executed successfully.';
 
